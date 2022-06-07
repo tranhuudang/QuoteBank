e9e1886fdcfac3bad47c98ecee414befb3f6693a
Tran Huu Dang <tranhuudang148@gmail.com>
2022-06-07 13:51:28 +0700

    add more vietnamese quote

--- QuoteBank/Quotes.cs
+++ QuoteBank/Quotes.cs
@@ -46,9 +46,51 @@
         string[] quote_vietnamese = {
             "Có công mài sắt có ngày nên kim.",
             "Một cây làm chẳng nên non, ba cây chụm lại nên hòn núi cao.",
-            "Công cha như núi thái sơn, nghĩa mẹ như nước trong nguồn chảy ra.",
-            "Bầu ơi thương lấy bí cùng, tuy rằng khác giống nhưng chung một giàn"
-        
+            "Ai ơi giữ chí cho bền, dù ai xoay hướng đổi nền mặc ai.",
+            "Hãy cho bền chí câu cua, dù ai câu chạch câu rùa mặc ai",
+            "Kiến tha lâu đầy tổ",
+            "Ngọc kia chuốt mãi cùng tròn",
+            "Năng nhặt chặt bị",
+            "Một lần ngại tốn, bốn lần chẳng xong",
+            "Dao có mài mới sắc, người có học mới nên.",
+            "Học là học để mà hành. Vừa hành vừa học mới thành người khôn.",
+            "Làm trai cố chí học hành, lập nên công nghiệp để dành mai sau.",
+            "Tất cả những khó khăn thường là để chuẩn bị cho những người bình thường một số phận phi thường.",
+            "Bí mật của cuộc sống là: Ngã bảy lần và đứng dậy tám lần.",
+            "Nếu cuộc đời ném vào mặt bạn một quả chanh, hãy vắt nước chanh thay vì chê nó chua quá.",
+            "Cách tốt nhất để cổ vũ bản thân là cố gắng cổ vũ người khác.",
+            "Hạnh phúc là một sự lựa chọn. Đau khổ cũng là một sự lựa chọn. Hãy lựa chọn khôn ngoan.",
+            "Hôm nay, tôi chọn là phiên bản tốt nhất của chính mình.",
+            "Hãy sống mỗi ngày như thể đó là ngày cuối cùng của bạn.",
+            "Hôm nay bạn ngồi dưới bóng mát, vì có người đã trồng cây từ lâu lắm rồi.",
+            "Cuộc sống là 10% những gì xảy ra với bạn và 90% cách bạn ứng phó với nó",
+            "Nỗi đau bạn cảm thấy hôm nay là sức mạnh bạn cảm thấy vào ngày mai.",
+            "Một tấm lòng biến ơn là nền tảng cho mọi đức tính tốt đẹp khác",
+            "Mỗi vị thánh đều có một quá khứ, mỗi tội nhân đều có một tương lai.",
+            "Đừng bao giờ ngừng tin rằng những điều tốt đẹp đang đến.",
+            "Có những ngày tuyệt vọng cùng cực, tôi và cuộc đời đã tha thứ cho nhau.",
+            "Khóc sẽ tốt hơn là tức giận. Vì tức giận làm mọi người tổn thương, còn nước mắt lặng lẽ gột rửa tâm hồn.",
+            "Thời gian như nước, phẳng lặng là tươi đẹp. Ngày tháng như sen, bình dị là tao nhã.",
+            "Hãy yêu người mình thích, ăn những gì mình thèm và làm những việc mà mình muốn. Sống mà, phải vui lên!",
+            "Lịch sử luôn được viết bởi người chiến thắng.",
+            "Nếu cơ hội không gõ cửa, hãy xây dựng một cánh cửa.",
+            "Không có thang máy dẫn đến thành công - bạn phải đi cầu thang bộ.",
+            "Đừng so sánh bản thân với người khác, hãy so sánh với chính bạn ngày hôm qua.",
+            "Đừng nằm trên giường quá lâu, trừ khi bạn làm ra tiền trên đó.",
+            "Suy nghĩ như ông chủ, hành xử như quý bà, làm việc như trâu ngựa, thành công sẽ không xa.",
+            "Khi bạn thành công, nói gì cũng đúng. Khi bạn thất bại, nói đúng thành sai.",
+            "Nếu chỉ đọc những cuốn sách người khác đã đọc, bạn sẽ chỉ biết những điều người khác đã biết.",
+            "Nếu bây giờ bạn không cố gắng, 10 năm nữa bạn vẫn là bạn, chỉ là già và xấu hơn thôi.",
+            "Thắng làm vua, thua làm giặc.",
+            "Nếu bạn muốn thứ bạn chưa từng có, bạn phải làm điều bạn chưa bao giờ làm.",
+            "Hai mươi năm nữa bạn sẽ thất vọng vì những điều mình không làm hơn là những điều bạn đã làm.",
+            "May mắn là khi sự chuẩn bị và thời cơ gặp nhau.",
+            "Người khôn nghĩ rồi mới nói, người ngu đần nói rồi mới nghĩ.",
+            "Đừng để bạn của tương lai chán ghét bản thân lúc này.",
+            "Đừng sợ việc tiến chậm, hãy sợ việc đứng yên.",
+            "Nếu bạn sinh ra trong nghèo khó, đó không phải lỗi của bạn. Nhưng nếu bạn chết trong nghèo đói, đó là lỗi của bạn.",
+            "Chăm chỉ đánh bại tài năng khi tài năng không chăm chỉ.",
+
         };
         /// <summary>
         /// Get a specific quote with given language
@@ -61,10 +103,10 @@
             string outQuote = "";
             switch (language)
             {
-                case "vi":
+                case "vi-VI":
                     outQuote = quote_vietnamese[number];
                     break;
-                case "en":
+                case "en-US":
                     outQuote = quote_english[number];
                     break;
                 default:
--- QuoteBank/README.md
+++ QuoteBank/README.md
@@ -1,8 +1,10 @@
-<p>In QuoteBank we have two method to get a quote out of library called: getQuote();</p>
-<p>You can get a random quote by provide region in the getQuote parameter: getQuote("us") or getQuote("vi").</p>
-<p>Example: </p>
-<br/>
-<p>using QuoteBank;</p>
-<p>...</p>
-<p>Quotes quotes = new Quotes();</p>
-<p>lbQuotation.Text = quotes.getQuote("en");</p>
+In QuoteBank we have two method to get a quote out of library called: 'getQuote();'.
+You can get a random quote by provide region in the getQuote parameter: 'getQuote("en-US")' for English or 'getQuote("vi-VI")' for Vietnamese. 
+In the future, we'll' try to cover more language support.
+Example:
+```
+using QuoteBank;
+...
+Quotes quotes = new Quotes();
+lbQuotation.Text = quotes.getQuote("en");
+```
