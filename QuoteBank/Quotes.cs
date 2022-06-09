c64135d3ef3c18343c9daae8b8bf92b6c6a03569
Tran Huu Dang <tranhuudang148@gmail.com>
2022-06-10 00:25:27 +0700

    add Chinese

--- QuoteBank/Quotes.cs
+++ QuoteBank/Quotes.cs
@@ -1,97 +1,14 @@
-﻿namespace QuoteBank
+﻿
+
+
+using QuoteBank.Languages;
+
+namespace QuoteBank
 {
     public class Quotes
     {
-        private string[] quote_english =
-       {
-            "Don’t wish it were easier; wish you were better.",
-            "There are no limits!",
-            "Courage + work = success",
-            "There are no shortcuts to any place worth going.",
-            "One day, all your hard work will pay off.",
-            "Do what is right, not what is easy.",
-            "It’s never too late to be what you might have been.",
-            "To change your life, you must first change your day.",
-            "Don’t stop until you’re proud.",
-            "You may see me struggle but you will never see me quit.",
-            "Work hard in silence. Let success make the noise.",
-            "Winners will fail over and over again until they succeed.",
-            "Allow yourself to be a beginner. No one starts off being excellent.",
-            "The man on top of the mountain didn’t fall there.",
-            "The difference between ordinary and extraordinary is that little extra.",
-            "Be so good they can’t ignore you.",
-            "Vision without execution is just hallucination.",
-            "If your dreams don’t scare you, they aren’t big enough.",
-            "The distance between dreams and reality is called action.",
-            "The thing about motivated people chasing their dream is they look crazy to lazy people.",
-            "If you have self-esteem, you give yourself the audacity to dream big.",
-            "Never let small minds convince you that your dreams are too big.",
-            "What we truly need to do is often what we most feel like avoiding.",
-            "Productivity boils down to two steps: identify the essential, then eliminate the rest.",
-            "The goal is not simply to get more done, but also to have less to do.",
-            "Hard work beats talent when talent doesn’t work hard.",
-            "Focus on being productive instead of busy.",
-            "Worrying is silly. It’s like wandering around with an umbrella waiting for it to rain.",
-            "Impossible is not a fact. It is an opinion.",
-            "Anyone who stops learning is old, whether at twenty or eighty. Anyone who keeps learning stays young.",
-            "The more that you read, the more things you will know. The more that you learn, the more places you’ll go.",
-            "Live as if you were to die tomorrow. Learn as if you were to live forever.",
-            "Being a student is easy. Learning requires actual work.",
-            "The beautiful thing about learning is nobody can take it away from you.",
-            "It is what we know already that often prevents us from learning.",
-            "I am learning all the time. The tombstone will be my diploma.",
-            "Education is the passport to the future, for tomorrow belongs to those who prepare for it today.",
 
-        };
-        string[] quote_vietnamese = {
-            "Có công mài sắt có ngày nên kim.",
-            "Một cây làm chẳng nên non, ba cây chụm lại nên hòn núi cao.",
-            "Ai ơi giữ chí cho bền, dù ai xoay hướng đổi nền mặc ai.",
-            "Hãy cho bền chí câu cua, dù ai câu chạch câu rùa mặc ai",
-            "Kiến tha lâu đầy tổ",
-            "Ngọc kia chuốt mãi cùng tròn",
-            "Năng nhặt chặt bị",
-            "Một lần ngại tốn, bốn lần chẳng xong",
-            "Dao có mài mới sắc, người có học mới nên.",
-            "Học là học để mà hành. Vừa hành vừa học mới thành người khôn.",
-            "Làm trai cố chí học hành, lập nên công nghiệp để dành mai sau.",
-            "Tất cả những khó khăn thường là để chuẩn bị cho những người bình thường một số phận phi thường.",
-            "Bí mật của cuộc sống là: Ngã bảy lần và đứng dậy tám lần.",
-            "Nếu cuộc đời ném vào mặt bạn một quả chanh, hãy vắt nước chanh thay vì chê nó chua quá.",
-            "Cách tốt nhất để cổ vũ bản thân là cố gắng cổ vũ người khác.",
-            "Hạnh phúc là một sự lựa chọn. Đau khổ cũng là một sự lựa chọn. Hãy lựa chọn khôn ngoan.",
-            "Hôm nay, tôi chọn là phiên bản tốt nhất của chính mình.",
-            "Hãy sống mỗi ngày như thể đó là ngày cuối cùng của bạn.",
-            "Hôm nay bạn ngồi dưới bóng mát, vì có người đã trồng cây từ lâu lắm rồi.",
-            "Cuộc sống là 10% những gì xảy ra với bạn và 90% cách bạn ứng phó với nó",
-            "Nỗi đau bạn cảm thấy hôm nay là sức mạnh bạn cảm thấy vào ngày mai.",
-            "Một tấm lòng biến ơn là nền tảng cho mọi đức tính tốt đẹp khác",
-            "Mỗi vị thánh đều có một quá khứ, mỗi tội nhân đều có một tương lai.",
-            "Đừng bao giờ ngừng tin rằng những điều tốt đẹp đang đến.",
-            "Có những ngày tuyệt vọng cùng cực, tôi và cuộc đời đã tha thứ cho nhau.",
-            "Khóc sẽ tốt hơn là tức giận. Vì tức giận làm mọi người tổn thương, còn nước mắt lặng lẽ gột rửa tâm hồn.",
-            "Thời gian như nước, phẳng lặng là tươi đẹp. Ngày tháng như sen, bình dị là tao nhã.",
-            "Hãy yêu người mình thích, ăn những gì mình thèm và làm những việc mà mình muốn. Sống mà, phải vui lên!",
-            "Lịch sử luôn được viết bởi người chiến thắng.",
-            "Nếu cơ hội không gõ cửa, hãy xây dựng một cánh cửa.",
-            "Không có thang máy dẫn đến thành công - bạn phải đi cầu thang bộ.",
-            "Đừng so sánh bản thân với người khác, hãy so sánh với chính bạn ngày hôm qua.",
-            "Đừng nằm trên giường quá lâu, trừ khi bạn làm ra tiền trên đó.",
-            "Suy nghĩ như ông chủ, hành xử như quý bà, làm việc như trâu ngựa, thành công sẽ không xa.",
-            "Khi bạn thành công, nói gì cũng đúng. Khi bạn thất bại, nói đúng thành sai.",
-            "Nếu chỉ đọc những cuốn sách người khác đã đọc, bạn sẽ chỉ biết những điều người khác đã biết.",
-            "Nếu bây giờ bạn không cố gắng, 10 năm nữa bạn vẫn là bạn, chỉ là già và xấu hơn thôi.",
-            "Thắng làm vua, thua làm giặc.",
-            "Nếu bạn muốn thứ bạn chưa từng có, bạn phải làm điều bạn chưa bao giờ làm.",
-            "Hai mươi năm nữa bạn sẽ thất vọng vì những điều mình không làm hơn là những điều bạn đã làm.",
-            "May mắn là khi sự chuẩn bị và thời cơ gặp nhau.",
-            "Người khôn nghĩ rồi mới nói, người ngu đần nói rồi mới nghĩ.",
-            "Đừng để bạn của tương lai chán ghét bản thân lúc này.",
-            "Đừng sợ việc tiến chậm, hãy sợ việc đứng yên.",
-            "Nếu bạn sinh ra trong nghèo khó, đó không phải lỗi của bạn. Nhưng nếu bạn chết trong nghèo đói, đó là lỗi của bạn.",
-            "Chăm chỉ đánh bại tài năng khi tài năng không chăm chỉ.",
-
-        };
+       
         /// <summary>
         /// Get a specific quote with given language
         /// </summary>
@@ -104,10 +21,13 @@
             switch (language)
             {
                 case "vi-VI":
-                    outQuote = quote_vietnamese[number];
+                    outQuote = vi_VI.quote_vietnamese[number];
                     break;
                 case "en-US":
-                    outQuote = quote_english[number];
+                    outQuote = en_US.quote_english[number];
+                    break;
+                case "zh-CN":
+                    outQuote = zh_CN.quote_chinese[number];
                     break;
                 default:
                     break;
@@ -125,11 +45,14 @@
             string outQuote = "";
             switch (language)
             {
-                case "vi":
-                    outQuote = quote_vietnamese[random.Next(quote_vietnamese.Length)];
+                case "vi-VI":
+                    outQuote = vi_VI.quote_vietnamese[random.Next(vi_VI.quote_vietnamese.Length)];
                     break;
-                case "en":
-                    outQuote = quote_english[random.Next(quote_english.Length)];
+                case "en-US":
+                    outQuote = en_US.quote_english[random.Next(en_US.quote_english.Length)];
+                    break;
+                case "zh-CN":
+                    outQuote = zh_CN.quote_chinese[random.Next(en_US.quote_english.Length)];
                     break;
                 default:
                     break;
--- QuoteBank/README.md
+++ QuoteBank/README.md
@@ -1,10 +1,11 @@
 * In QuoteBank we have two method to get a quote out of library called: 'getQuote();'.
-* You can get a random quote by provide region in the getQuote parameter: 'getQuote("en-US")' for English or 'getQuote("vi-VI")' for Vietnamese. 
+* You can get a random quote by provide region in the getQuote parameter: 'getQuote("en-US")' for English. 
 * In the future, we'll' try to cover more language support.
+* Supported language: English(en-US), Vietnamese(vi-VI).
 * Example:
 ```
 using QuoteBank;
 ...
 Quotes quotes = new Quotes();
-lbQuotation.Text = quotes.getQuote("en");
+lbQuotation.Text = quotes.getQuote("en-US");
 ```
